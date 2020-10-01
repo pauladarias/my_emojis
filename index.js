9513406dbd0424c9a4c0076b2dffae7dd70f47e0
pauladarias <pauladarias@gmail.com>
2020-10-01 13:05:21 +0100

    Add push and unshift event listeners

--- index.js
+++ index.js
@@ -2,18 +2,34 @@ const myEmojis = ["👨‍💻", "⛷", "🍲"];
 const emojiContainer = document.getElementById("emoji-container");
 const input = document.getElementById("emoji-input");
 const pushBtn = document.getElementById("push-btn");
+const unshiftBtn = document.getElementById("unshift-btn");
+const popBtn = document.getElementById("pop-btn");
+const shiftBtn = document.getElementById("shift-btn");
 
-for (let i = 0; i < myEmojis.length; i++) {
-  let span = document.createElement("span");
-  span.textContent = myEmojis[i];
-  emojiContainer.append(span);
+function renderEmoji() {
+  for (let i = 0; i < myEmojis.length; i++) {
+    let span = document.createElement("span");
+    span.textContent = myEmojis[i];
+    emojiContainer.append(span);
+  }
 }
 
+renderEmoji();
+
 pushBtn.addEventListener("click", function () {
-  if (input.value != "") {
+  if (input.value) {
     myEmojis.push(input.value);
-    console.log(myEmojis);
     input.value = "";
-    emojiContainer.textContent = myEmojis;
+    emojiContainer.innerHTML = "";
+    renderEmoji();
+  }
+});
+
+unshiftBtn.addEventListener("click", function () {
+  if (input.value) {
+    myEmojis.unshift(input.value);
+    input.value = "";
+    emojiContainer.innerHTML = "";
+    renderEmoji();
   }
 });
