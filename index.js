0eb84147188809766a9c37fe33878e3be10cd24b
pauladarias <pauladarias@gmail.com>
2020-10-01 11:55:02 +0100

    Get input value

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const myEmojis = ["👨‍💻", "⛷", "🍲"];
-const emojiContainer = document.getElementById("emojiContainer");
-console.log(emojiContainer);
+const emojiContainer = document.getElementById("emoji-container");
+const input = document.getElementById("emoji-input");
+const pushBtn = document.getElementById("push-btn");
 
 for (let i = 0; i < myEmojis.length; i++) {
   let span = document.createElement("span");
+  span.textContent = myEmojis[i];
   emojiContainer.append(span);
 }
+
+pushBtn.addEventListener("click", function () {
+  console.log(input.value);
+});
